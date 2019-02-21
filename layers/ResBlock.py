b085f11f0836b81bb7c11d7f9fba9b03f37849da
Valentin Durand de Gevigney <valentin.durand.de.g@gmail.com>
2019-02-21 18:41:12 +0100

    Updated ResBlock to reflect things learned with DenseBlock

--- layers/ResBlock.py
+++ layers/ResBlock.py
@@ -1,15 +1,19 @@
-from keras.layers import Layer, InputSpec, BatchNormalization
+from keras.layers import InputSpec, BatchNormalization
+from keras.layers import Conv1D, Conv2D, Conv3D
+from keras.layers import Deconv2D, Deconv3D
 from keras.utils import conv_utils
+from keras.utils.generic_utils import to_list
 from keras import activations, initializers, regularizers, constraints
 from keras import backend as K
 
-RES_DEPTH = 2
+from layers import CompositeLayer
 
 
-class _ResBlock(Layer):
+class _ResBlock(CompositeLayer):
     def __init__(self, rank,
                  filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  dilation_rate=1,
@@ -26,12 +30,13 @@ class _ResBlock(Layer):
                  **kwargs):
 
         assert rank in [1, 2, 3]
+        assert depth > 0
 
         super(_ResBlock, self).__init__(**kwargs)
         self.rank = rank
         self.filters = filters
         self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank, "kernel_size")
-        self.projection_kernel_size = conv_utils.normalize_tuple(1, rank, "projection_kernel_size")
+        self.depth = depth
         self.strides = conv_utils.normalize_tuple(strides, rank, "strides")
         self.data_format = K.normalize_data_format(data_format)
         self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank, "dilation_rate")
@@ -46,74 +51,96 @@ class _ResBlock(Layer):
         self.bias_constraint = constraints.get(bias_constraint)
         self.use_batch_normalization = use_batch_normalization
 
-        self.kernels = []
-        self.projection_kernel = None
-        self.biases = []
+        self.conv_layers = []
+        self.batch_normalization_layers = []
+        self.projection_layer = None
 
         self.input_spec = InputSpec(ndim=self.rank + 2)
 
+    def get_conv_layer_type(self):
+        return Conv1D if self.rank is 1 else Conv2D if self.rank is 2 else Conv3D
+
+    def init_layers(self, input_shape):
+        conv_layer_type = self.get_conv_layer_type()
+        for i in range(self.depth):
+            strides = self.get_strides_at_depth(i)
+            conv_layer = conv_layer_type(filters=self.filters,
+                                         kernel_size=self.kernel_size,
+                                         strides=strides,
+                                         padding="same",
+                                         data_format=self.data_format,
+                                         dilation_rate=self.dilation_rate,
+                                         use_bias=self.use_bias,
+                                         kernel_initializer=self.kernel_initializer,
+                                         bias_initializer=self.bias_initializer,
+                                         kernel_regularizer=self.kernel_regularizer,
+                                         bias_regularizer=self.bias_regularizer,
+                                         activity_regularizer=self.activity_regularizer,
+                                         kernel_constraint=self.kernel_constraint,
+                                         bias_constraint=self.bias_constraint)
+            self.conv_layers.append(conv_layer)
+
+            if self.use_batch_normalization:
+                self.batch_normalization_layers.append(BatchNormalization())
+
+        if self.use_projection(input_shape):
+            projection_kernel_size = conv_utils.normalize_tuple(1, self.rank, "projection_kernel_size")
+            self.projection_layer = conv_layer_type(filters=self.filters,
+                                                    kernel_size=projection_kernel_size,
+                                                    strides=self.strides,
+                                                    padding="same",
+                                                    data_format=self.data_format,
+                                                    dilation_rate=self.dilation_rate,
+                                                    use_bias=False,
+                                                    kernel_initializer=self.kernel_initializer,
+                                                    bias_initializer=self.bias_initializer,
+                                                    kernel_regularizer=self.kernel_regularizer,
+                                                    bias_regularizer=self.bias_regularizer,
+                                                    activity_regularizer=self.activity_regularizer,
+                                                    kernel_constraint=self.kernel_constraint,
+                                                    bias_constraint=self.bias_constraint)
+
     def build(self, input_shape):
-        input_dim = self.get_input_dim(input_shape)
+        self.init_layers(input_shape)
+        intermediate_shape = input_shape
+
         with K.name_scope("residual_block_weights"):
-            for i in range(RES_DEPTH):
-                with K.name_scope("residual_layer_weights_{}".format(i)):
-                    self.add_kernel(input_dim if (i == 0) else self.filters)
-                    if self.use_bias:
-                        self.add_bias()
+            for i in range(self.depth):
+                self.build_sub_layer(self.conv_layers[i], intermediate_shape)
+                intermediate_shape = self.conv_layers[i].compute_output_shape(intermediate_shape)
 
-        with K.name_scope("projection_kernel_weights"):
-            if self.use_projection(input_shape):
-                self.projection_kernel = self.add_weight(shape=self.get_kernel_shape(input_dim, True),
-                                                         initializer=self.kernel_initializer,
-                                                         name="projection_kernel",
-                                                         regularizer=self.kernel_regularizer,
-                                                         constraint=self.kernel_constraint)
+            if self.projection_layer is not None:
+                self.build_sub_layer(self.projection_layer, input_shape)
 
-        self.input_spec = InputSpec(ndim=self.rank + 2, axes={self.channel_axis: self.get_input_dim(input_shape)})
+        self.input_spec = InputSpec(ndim=self.rank + 2, axes={self.channel_axis: input_shape[self.channel_axis]})
         super(_ResBlock, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
-        conv = None
-        if self.rank == 1:
-            conv = K.conv1d
-        elif self.rank == 2:
-            conv = K.conv2d
-        elif self.rank == 3:
-            conv = K.conv3d
-
         outputs = inputs
-        for i in range(RES_DEPTH):
+        for i in range(self.depth):
             with K.name_scope("residual_layer_{}".format(i)):
-                strides = self.get_strides_at_depth(i)
-
                 if self.use_batch_normalization:
-                    outputs = BatchNormalization()(outputs)
+                    outputs = self.batch_normalization_layers[i](outputs)
 
                 if self.activation is not None:
                     outputs = self.activation(outputs)
 
-                outputs = conv(outputs,
-                               self.kernels[i],
-                               strides=strides,
-                               padding="same",
-                               data_format=self.data_format,
-                               dilation_rate=self.dilation_rate)
-
-                if self.use_bias:
-                    outputs = K.bias_add(outputs,
-                                         self.biases[i],
-                                         data_format=self.data_format)
+                outputs = self.conv_layers[i](outputs)
 
         if self.use_projection(K.int_shape(inputs)):
-            inputs = conv(inputs, self.projection_kernel, strides=self.strides, padding="same")
+            inputs = self.projection_layer(inputs)
 
         # x_k+1 = x_k + f(x_k)
         outputs = K.sum([outputs, inputs], axis=0)
         return outputs
 
     def use_projection(self, input_shape):
-        assert None not in input_shape[1:], "Cannot decide if projection is needed without knowing input shape (TO FIX)"
-        return input_shape != self.compute_output_shape(input_shape)
+        strides = to_list(self.strides, allow_tuple=True)
+        for stride in strides:
+            if stride != 1:
+                return True
+
+        return input_shape[self.channel_axis] != self.filters
 
     def compute_output_shape(self, input_shape):
         def get_new_space(space):
@@ -145,29 +172,9 @@ class _ResBlock(Layer):
         return self.data_format == "channels_first"
 
     def get_input_dim(self, input_shape):
-        assert input_shape[self.channel_axis] is not None,\
+        assert input_shape[self.channel_axis] is not None, \
             "The channel dimension of the inputs should be defined. Found `None`."
         return input_shape[self.channel_axis]
-
-    def get_kernel_shape(self, input_dim, for_projection_kernel=False):
-        kernel_size = self.kernel_size if not for_projection_kernel else self.projection_kernel_size
-        return kernel_size + (input_dim, self.filters)
-
-    def add_kernel(self, input_dim):
-        kernel = self.add_weight(shape=self.get_kernel_shape(input_dim),
-                                 initializer=self.kernel_initializer,
-                                 name="kernel_{0}".format(len(self.kernels)),
-                                 regularizer=self.kernel_regularizer,
-                                 constraint=self.kernel_constraint)
-        self.kernels += [kernel]
-
-    def add_bias(self):
-        bias = self.add_weight(shape=(self.filters,),
-                               initializer=self.bias_initializer,
-                               name="bias_{0}".format(len(self.biases)),
-                               regularizer=self.bias_regularizer,
-                               constraint=self.bias_constraint)
-        self.biases += [bias]
 
     def get_strides_at_depth(self, depth):
         if depth == 0:
@@ -182,7 +189,7 @@ class _ResBlock(Layer):
                 "rank": self.rank,
                 "filters": self.filters,
                 "kernel_size": self.kernel_size,
-                "depth": RES_DEPTH,
+                "depth": self.depth,
                 "strides": self.strides,
                 "padding": "same",
                 "data_format": self.data_format,
@@ -204,6 +211,7 @@ class _ResBlock(Layer):
 class ResBlock1D(_ResBlock):
     def __init__(self, filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  dilation_rate=1,
@@ -219,7 +227,7 @@ class ResBlock1D(_ResBlock):
                  use_batch_normalization=True,
                  **kwargs):
         super(ResBlock1D, self).__init__(rank=1,
-                                         filters=filters, kernel_size=kernel_size,
+                                         filters=filters, kernel_size=kernel_size, depth=depth,
                                          strides=strides, data_format=data_format,
                                          dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
                                          kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
@@ -238,6 +246,7 @@ class ResBlock1D(_ResBlock):
 class ResBlock2D(_ResBlock):
     def __init__(self, filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  dilation_rate=1,
@@ -253,7 +262,7 @@ class ResBlock2D(_ResBlock):
                  use_batch_normalization=True,
                  **kwargs):
         super(ResBlock2D, self).__init__(rank=2,
-                                         filters=filters, kernel_size=kernel_size,
+                                         filters=filters, kernel_size=kernel_size, depth=depth,
                                          strides=strides, data_format=data_format,
                                          dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
                                          kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
@@ -272,6 +281,7 @@ class ResBlock2D(_ResBlock):
 class ResBlock3D(_ResBlock):
     def __init__(self, filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  dilation_rate=1,
@@ -287,7 +297,7 @@ class ResBlock3D(_ResBlock):
                  use_batch_normalization=True,
                  **kwargs):
         super(ResBlock3D, self).__init__(rank=3,
-                                         filters=filters, kernel_size=kernel_size,
+                                         filters=filters, kernel_size=kernel_size, depth=depth,
                                          strides=strides, data_format=data_format,
                                          dilation_rate=dilation_rate, activation=activation, use_bias=use_bias,
                                          kernel_initializer=kernel_initializer, bias_initializer=bias_initializer,
@@ -304,77 +314,9 @@ class ResBlock3D(_ResBlock):
 
 
 class _ResBlockTranspose(_ResBlock):
-    def get_kernel_shape(self, input_dim, for_projection_kernel=False):
-        kernel_size = self.kernel_size if not for_projection_kernel else self.projection_kernel_size
-        return kernel_size + (self.filters, input_dim)
-
-    def call(self, inputs, **kwargs):
-        deconv = None
-        if self.rank == 2:
-            deconv = K.conv2d_transpose
-        elif self.rank == 3:
-            deconv = K.conv3d_transpose
-
-        outputs = inputs
-        projection_output_shape = None
-        for i in range(RES_DEPTH):
-            strides = self.get_strides_at_depth(i)
-
-            if self.use_batch_normalization:
-                outputs = BatchNormalization()(outputs)
-
-            if self.activation is not None:
-                outputs = self.activation(outputs)
-
-            deconv_output_shape = self.get_deconv_output_shape(outputs, i)
-            if i == 0:
-                projection_output_shape = deconv_output_shape
-
-            outputs = deconv(
-                outputs,
-                self.kernels[i],
-                deconv_output_shape,
-                strides=strides,
-                padding="same",
-                data_format=self.data_format)
-
-            if self.use_bias:
-                outputs = K.bias_add(
-                    outputs,
-                    self.biases[i],
-                    data_format=self.data_format)
-
-        if self.use_projection(K.int_shape(inputs)):
-            inputs = deconv(inputs, self.projection_kernel, projection_output_shape,
-                            strides=self.strides, padding="same")
-
-        # x_k+1 = x_k + f(x_k)
-        outputs = K.sum([outputs, inputs], axis=0)
-        return outputs
-
-    def get_deconv_output_shape(self, inputs, depth):
-        strides = self.get_strides_at_depth(depth)
-
-        input_shape = K.shape(inputs)
-        batch_size = input_shape[0]
-
-        if self.channels_first:
-            spatial_axis = list(range(2, self.rank + 2))
-        else:
-            spatial_axis = list(range(1, self.rank + 1))
-
-        inputs_spatial_dims = [input_shape[axis] for axis in spatial_axis]
-        out_spatial_dims = []
-        for i in range(len(inputs_spatial_dims)):
-            out_dim = conv_utils.deconv_length(inputs_spatial_dims[i], strides[i], self.kernel_size[i],
-                                               padding="same", output_padding=None, dilation=self.dilation_rate[i])
-            out_spatial_dims.append(out_dim)
-
-        if self.channels_first:
-            output_shape = (batch_size, self.filters, *out_spatial_dims)
-        else:
-            output_shape = (batch_size, *out_spatial_dims, self.filters)
-        return output_shape
+    def get_conv_layer_type(self):
+        assert self.rank in [2, 3]
+        return Deconv2D if self.rank is 2 else Deconv3D
 
     def compute_output_shape(self, input_shape):
         def get_new_space(space):
@@ -398,6 +340,7 @@ class _ResBlockTranspose(_ResBlock):
 class ResBlock2DTranspose(_ResBlockTranspose):
     def __init__(self, filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  activation="relu",
@@ -412,7 +355,7 @@ class ResBlock2DTranspose(_ResBlockTranspose):
                  use_batch_normalization=True,
                  **kwargs):
         super(ResBlock2DTranspose, self).__init__(rank=2,
-                                                  filters=filters, kernel_size=kernel_size,
+                                                  filters=filters, kernel_size=kernel_size, depth=depth,
                                                   strides=strides, data_format=data_format,
                                                   activation=activation, use_bias=use_bias,
                                                   kernel_initializer=kernel_initializer,
@@ -433,6 +376,7 @@ class ResBlock2DTranspose(_ResBlockTranspose):
 class ResBlock3DTranspose(_ResBlockTranspose):
     def __init__(self, filters,
                  kernel_size,
+                 depth=2,
                  strides=(1, 1),
                  data_format=None,
                  activation="relu",
@@ -447,7 +391,7 @@ class ResBlock3DTranspose(_ResBlockTranspose):
                  use_batch_normalization=True,
                  **kwargs):
         super(ResBlock3DTranspose, self).__init__(rank=3,
-                                                  filters=filters, kernel_size=kernel_size,
+                                                  filters=filters, kernel_size=kernel_size, depth=depth,
                                                   strides=strides, data_format=data_format,
                                                   activation=activation, use_bias=use_bias,
                                                   kernel_initializer=kernel_initializer,
