Improved performance for BrightnessShifter Added test_dataset to evaluated samples for normalization Minor bugs fixed

data_preprocessors/BrightnessShifter.py
@@ -18,32 +18,28 @@ class BrightnessShifter(DataPreprocessor):
         self.apply_on_outputs = apply_on_outputs
 
     def process(self, inputs: np.ndarray, outputs: np.ndarray):
-        if self.apply_on_outputs:
-            inputs_len = inputs.shape[1]
-            array = np.concatenate([inputs, outputs], axis=1)
-            array = self.process_one(array)
-            inputs = array[:, :inputs_len]
-            outputs = array[:, inputs_len:]
-        else:
-            inputs = self.process_one(inputs)
-
-        return inputs, outputs
-
-    def process_one(self, array: np.ndarray):
-        size = [len(array)] + [1] * (array.ndim - 1)
+        size = [len(inputs)] + [1] * (inputs.ndim - 1)
         if self.gain is not None:
             gain = random_range_value(self.gain, size=size)
-            array = array * gain
+            inputs = inputs * gain
+            if self.apply_on_outputs:
+                outputs = outputs * gain
 
         if self.bias is not None:
             bias = random_range_value(self.bias, center=0.0, size=size)
-            array = array + bias
+            inputs = inputs + bias
+            if self.apply_on_outputs:
+                outputs = outputs + bias
 
         if (self.gain is not None) or (self.bias is not None):
             if self.normalization_method == "clip":
-                array = np.clip(array, self.values_range[0], self.values_range[1])
+                inputs = np.clip(inputs, self.values_range[0], self.values_range[1])
+                if self.apply_on_outputs:
+                    outputs = np.clip(outputs, self.values_range[0], self.values_range[1])
             elif self.normalization_method == "mod":
                 mod_range = self.values_range[1] - self.values_range[0]
-                array = np.mod(array, mod_range) + self.values_range[0]
+                inputs = np.mod(inputs, mod_range) + self.values_range[0]
+                if self.apply_on_outputs:
+                    outputs = np.mod(outputs, mod_range) + self.values_range[0]
 
-        return array
+        return inputs, outputs

models/AutoEncoderBaseModel.py
@@ -5,7 +5,7 @@ from keras.initializers import VarianceScaling
 from keras.regularizers import l2
 from keras.optimizers import Adam, RMSprop
 from keras.callbacks import TensorBoard, CallbackList, Callback, ProgbarLogger, BaseLogger, LearningRateScheduler
-from keras.backend import binary_crossentropy
+from keras.backend import binary_crossentropy, get_session
 from keras.utils.generic_utils import to_list
 import tensorflow as tf
 import numpy as np
@@ -570,7 +570,9 @@ class AutoEncoderBaseModel(ABC):
     def get_reconstruction_loss(self, reconstruction_metric_name: str):
         if self.temporal_loss_weights is None:
             reconstruction_loss_weights = np.ones([self.input_sequence_length], dtype=np.float32)
-            prediction_loss_weights = np.arange(start=1.0, stop=0.0, step=-1 / self.input_sequence_length,
+            stop = 0.2
+            step = (stop - 1) / self.input_sequence_length
+            prediction_loss_weights = np.arange(start=1.0, stop=stop, step=step,
                                                 dtype=np.float32)
             loss_weights = np.concatenate([reconstruction_loss_weights, prediction_loss_weights])
             self.temporal_loss_weights = tf.constant(loss_weights)
@@ -874,13 +876,13 @@ class AutoEncoderBaseModel(ABC):
             placeholder_shape = [None, *predictions.shape[1:]]
             self._auc_predictions_placeholder = tf.placeholder(dtype=tf.float32, shape=placeholder_shape)
             self._auc_labels_placeholder = tf.placeholder(dtype=tf.bool, shape=placeholder_shape)
-            roc_op = tf.metrics.auc(self._auc_labels_placeholder, self._auc_predictions_placeholder, "ROC",
+            roc_op = tf.metrics.auc(self._auc_labels_placeholder, self._auc_predictions_placeholder, curve="ROC",
                                     summation_method="careful_interpolation")
-            pr_op = tf.metrics.auc(self._auc_labels_placeholder, self._auc_predictions_placeholder, "PR",
+            pr_op = tf.metrics.auc(self._auc_labels_placeholder, self._auc_predictions_placeholder, curve="PR",
                                    summation_method="careful_interpolation")
             self._auc_ops = [roc_op, pr_op]
 
-        session: tf.Session = tf.get_default_session()
+        session: tf.Session = get_session()
         session.run(tf.local_variables_initializer())
         (_, roc), (_, pr) = session.run(self._auc_ops, feed_dict={self._auc_predictions_placeholder: predictions,
                                                                   self._auc_labels_placeholder: labels})