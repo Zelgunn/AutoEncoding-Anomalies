Main change : Updated test loops to use the new API Corrected normalization for labels and computation of size of modalities when extracted from tfrecord Separated the usual training dataset and a browser going through sources in order Created a RawPredictionsLayer to put predictions in a keras model Updated raw predictions model to use RawPredictionsLayer

datasets/loaders/DatasetConfig.py
@@ -11,7 +11,7 @@ def get_shard_count(sample_length: int,
                     shard_size: int
                     ) -> int:
     shard_count = 1 + np.ceil((sample_length - 1) / shard_size).astype(np.int)
-    return max(1, shard_count)
+    return max(2, shard_count)
 
 
 class DatasetConfig(object):

datasets/loaders/SubsetLoader.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.python.keras.backend import get_session
 import numpy as np
 import os
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple, Optional, Type, NamedTuple
 
 from datasets.loaders import DatasetConfig
 from modalities import Modality, ModalityCollection
@@ -11,6 +11,12 @@ from modalities import Modality, ModalityCollection
 def get_shard_count(sample_length, shard_size):
     shard_count = 1 + np.ceil((sample_length - 1) / shard_size).astype(np.int)
     return max(1, shard_count)
+
+
+class BrowserIterator(NamedTuple):
+    base_iterator: tf.data.Iterator
+    initializer: tf.Operation
+    iterator_next: Dict[str, tf.Tensor]
 
 
 class SubsetLoader(object):
@@ -36,16 +42,7 @@ class SubsetLoader(object):
         self._train_iterators: Dict[int, tf.data.Iterator] = {}
         self._test_iterators: Dict[int, tf.data.Iterator] = {}
 
-    def shard_filepath_generator(self):
-        while True:
-            source_index = np.random.randint(len(self.subset_folders))
-            source_folder = self.subset_folders[source_index]
-            shards = self.subset_files[source_folder]
-            offset = np.random.randint(len(shards) - self.shard_count_per_sample + 1)
-            shards_filepath = [os.path.join(source_folder, shard)
-                               for shard in shards[offset:offset + self.shard_count_per_sample]]
-            for filepath in shards_filepath:
-                yield filepath
+        self._source_browsers_iterators: Dict[int, BrowserIterator] = {}
 
     # region Loading methods (parsing, decoding, fusing, normalization, splitting, ...)
     def parse_shard(self, serialized_example, output_labels):
@@ -116,41 +113,66 @@ class SubsetLoader(object):
 
     def join_shards(self,
                     shards: Dict[str, tf.Tensor],
-                    shard_sizes: Dict[str, tf.Tensor]):
+                    shard_sizes: Dict[str, tf.Tensor],
+                    offset: tf.Tensor,
+                    sample_length: str = "output_length"):
+
         joint_shards = {}
+        labels_range = None
+        labels_offset = None
+
         for modality in self.modalities:
             modality_type = type(modality)
             modality_id = modality.tfrecord_id()
             modality_shards = shards[modality_id]
             modality_shard_sizes = shard_sizes[modality_id]
 
-            total_size = tf.cast(tf.reduce_sum(modality_shard_sizes), tf.int64, name="total_shard_size")
-            modality_sample_size = self.modalities[modality_type].io_shape.sample_length
-            modality_max_size = self.config.get_modality_max_shard_size(modality)
-            size_delta = total_size - modality_sample_size
-            offset = tf.random.uniform(shape=(), minval=0, maxval=size_delta, dtype=tf.int64, name="offset")
+            with tf.name_scope(modality_id):
+                total_size = tf.cast(tf.reduce_sum(modality_shard_sizes), tf.int32, name="total_shard_size")
 
-            modality_shards_shape = [self.shard_count_per_sample, modality_max_size] + [None] * (modality.rank() - 1)
-            modality_shards.set_shape(modality_shards_shape)
-            modality_shards = tf.unstack(modality_shards, axis=0)
-            modality_shards = tf.concat(modality_shards, axis=0)
-            modality_shards = modality_shards[offset:offset + modality_sample_size]
+                if sample_length == "output_length":
+                    modality_sample_size = self.modalities[modality_type].io_shape.output_length
+                else:
+                    modality_sample_size = self.modalities[modality_type].io_shape.input_length
+                modality_sample_size = tf.constant(modality_sample_size, name="modality_sample_size")
 
-            joint_shards[modality_id] = modality_shards
+                modality_effective_size = tf.cast(modality_shard_sizes[0], tf.float32, name="modality_effective_size")
+                modality_offset = tf.cast(offset * modality_effective_size, tf.int32, name="offset")
+
+                modality_shards_shape = tf.unstack(tf.shape(modality_shards), name="modality_shape")
+                shard_count, modality_size, *modality_shape = modality_shards_shape
+                modality_shards_shape = [shard_count * modality_size, *modality_shape]
+                modality_shards = tf.reshape(modality_shards, modality_shards_shape, "concatenate_shards")
+
+                modality_shards = modality_shards[modality_offset:modality_offset + modality_sample_size]
+                joint_shards[modality_id] = modality_shards
+
+            if "labels" in shards and labels_range is None:
+                labels_range = tf.cast(modality_sample_size, tf.float32) / tf.cast(total_size, tf.float32)
+                size_ratio = tf.cast(modality_effective_size, tf.float32) / tf.cast(total_size, tf.float32)
+                labels_offset = offset * size_ratio
 
         if "labels" in shards:
-            labels = shards["labels"]
-            shard_labels_offset = tf.range(self.shard_count_per_sample, dtype=tf.float32)
-            shard_labels_offset = tf.reshape(shard_labels_offset, [self.shard_count_per_sample, 1])
-            labels = (labels + shard_labels_offset) / self.shard_count_per_sample
+            labels: tf.Tensor = shards["labels"]
+
+            shard_labels_offset = tf.range(self.shards_per_sample, dtype=tf.float32, name="shard_labels_offset")
+            shard_labels_offset = tf.expand_dims(shard_labels_offset, axis=-1)
+
+            labels = (labels + shard_labels_offset) / self.shards_per_sample
+            labels = tf.clip_by_value(labels, labels_offset, labels_offset + labels_range)
+            labels = (labels - labels_offset) / labels_range
+
             labels = tf.reshape(labels, shape=[-1, 2])
             joint_shards["labels"] = labels
 
         return joint_shards
 
-    # TODO : Normalize according to config (model output)
+    # TODO : Normalize target range according to model config (model output)
     def normalize_batch(self, modalities: Dict[str, tf.Tensor]):
         for modality_id in modalities:
+            if modality_id == "labels":
+                continue
+
             modality_value = modalities[modality_id]
             modality_min, modality_max = self.config.modalities_ranges[modality_id]
             modalities[modality_id] = (modality_value - modality_min) / (modality_max - modality_min)
@@ -160,6 +182,8 @@ class SubsetLoader(object):
     def split_batch_io(self, modalities: Dict[str, tf.Tensor]):
         inputs, outputs = [], []
         for modality_id, modality_value in modalities.items():
+            if modality_id == "labels":
+                continue
             modality_type = ModalityCollection.modality_id_to_class(modality_id)
             input_length = self.modalities[modality_type].io_shape.input_length
             output_length = self.modalities[modality_type].io_shape.output_length
@@ -169,18 +193,35 @@ class SubsetLoader(object):
 
     # endregion
 
+    # region Random sampling
+    def random_shard_filepath_generator(self):
+        while True:
+            source_index = np.random.randint(len(self.subset_folders))
+            source_folder = self.subset_folders[source_index]
+            shards = self.subset_files[source_folder]
+            offset = np.random.randint(len(shards) - self.shards_per_sample + 1)
+            shards_filepath = [os.path.join(source_folder, shard)
+                               for shard in shards[offset:offset + self.shards_per_sample]]
+            for filepath in shards_filepath:
+                yield filepath
+
+    def join_shards_randomly(self,
+                             shards: Dict[str, tf.Tensor],
+                             shard_sizes: Dict[str, tf.Tensor]):
+        offset = tf.random.uniform(shape=(), minval=0, maxval=1.0, dtype=tf.float32, name="offset")
+        return self.join_shards(shards, shard_sizes, offset)
+
     def make_tf_dataset(self, output_labels: bool):
-        dataset = tf.data.Dataset.from_generator(self.shard_filepath_generator,
+        dataset = tf.data.Dataset.from_generator(self.random_shard_filepath_generator,
                                                  output_types=tf.string,
                                                  output_shapes=())
 
         dataset = tf.data.TFRecordDataset(dataset)
         dataset = dataset.map(lambda serialized_shard: self.parse_shard(serialized_shard, output_labels))
-        dataset = dataset.batch(self.shard_count_per_sample)
-        dataset = dataset.map(self.join_shards)
+        dataset = dataset.batch(self.shards_per_sample)
+        dataset = dataset.map(self.join_shards_randomly)
         dataset = dataset.map(self.normalize_batch)
         dataset = dataset.map(self.split_batch_io)
-        # dataset = dataset.batch(batch_size)
 
         return dataset
 
@@ -192,32 +233,122 @@ class SubsetLoader(object):
 
         return iterators[batch_size]
 
+    # endregion
+
+    # region Get batch
     def get_batch(self, batch_size: int, output_labels: bool):
         iterator = self.get_one_shot_iterator(batch_size, output_labels)
         session = get_session()
-        batch = session.run(iterator.get_next())
-        return batch
+        inputs, outputs = session.run(iterator.get_next())
+        return inputs, outputs
+
+    # endregion
+
+    # region Read dataset in order
+    def make_source_filepath_generator(self, source_index: int):
+        def generator():
+            source_folder = self.subset_folders[source_index]
+            shards_filepaths = [os.path.join(source_folder, shard) for shard in self.subset_files[source_folder]]
+            for i in range(len(shards_filepaths) - 1):
+                for filepath in shards_filepaths[i:i + 2]:
+                    yield filepath
+
+        return generator
+
+    def join_shards_ordered(self,
+                            shards: Dict[str, tf.Tensor],
+                            shard_sizes: Dict[str, tf.Tensor],
+                            reference_modality: Type[Modality],
+                            stride: int
+                            ):
+        reference_modality_id = reference_modality.tfrecord_id()
+        size = shard_sizes[reference_modality_id][0]
+        stride = tf.constant(stride, tf.int32, name="stride")
+        result_count = size // stride
+
+        def loop_body(i, step_shards_arrays: Dict[str, tf.TensorArray]):
+            step_offset = tf.cast(i * stride, tf.float32) / tf.cast(size, tf.float32)
+            step_joint_shards = self.join_shards(shards, shard_sizes, step_offset, sample_length="input_length")
+            for modality_id in step_shards_arrays:
+                modality = step_joint_shards[modality_id]
+                step_shards_arrays[modality_id] = step_shards_arrays[modality_id].write(i, modality)
+            i += 1
+            return i, step_shards_arrays
+
+        i_initializer = tf.constant(0, tf.int32)
+        shards_arrays = {modality_id: tf.TensorArray(dtype=shards[modality_id].dtype, size=result_count)
+                         for modality_id in shards}
+        results = tf.while_loop(cond=lambda i, _: i < result_count,
+                                body=loop_body,
+                                loop_vars=[i_initializer, shards_arrays],
+                                parallel_iterations=1)
+        joint_shard: Dict[str, tf.TensorArray] = results[1]
+        joint_shard = {modality_id: joint_shard[modality_id].stack() for modality_id in joint_shard}
+        return joint_shard
+
+    def get_source_browser(self,
+                           source_index: int,
+                           reference_modality: Type[Modality],
+                           stride: int) -> tf.data.Dataset:
+
+        dataset = tf.data.Dataset.from_generator(self.make_source_filepath_generator(source_index),
+                                                 output_types=tf.string,
+                                                 output_shapes=())
+
+        dataset = tf.data.TFRecordDataset(dataset)
+        dataset = dataset.map(lambda serialized_shard: self.parse_shard(serialized_shard, output_labels=True))
+        dataset = dataset.batch(self.shards_per_sample)
+        dataset = dataset.map(lambda shards, shard_sizes:
+                              self.join_shards_ordered(shards, shard_sizes, reference_modality, stride))
+        dataset = dataset.apply(tf.data.experimental.unbatch())
+        dataset = dataset.map(self.normalize_batch)
+        dataset = dataset.batch(1)
+
+        return dataset
+
+    def get_source_browser_iterator(self,
+                                    source_index: int,
+                                    reference_modality: Type[Modality],
+                                    stride: int
+                                    ):
+        if source_index not in self._source_browsers_iterators:
+            tf_dataset = self.get_source_browser(source_index, reference_modality, stride)
+            tf_dataset = tf_dataset.map(lambda dictionary: tuple(dictionary.values()))
+            base_iterator = tf_dataset.make_initializable_iterator()
+            iterator_next = base_iterator.get_next()
+            iterator = BrowserIterator(base_iterator=base_iterator,
+                                       initializer=base_iterator.initializer,
+                                       iterator_next=iterator_next)
+            self._source_browsers_iterators[source_index] = iterator
+
+        return self._source_browsers_iterators[source_index]
+
+    # endregion
 
     # region Properties
     @property
-    def train_tf_dataset(self):
+    def train_tf_dataset(self) -> tf.data.Dataset:
         if self._train_tf_dataset is None:
             self._train_tf_dataset = self.make_tf_dataset(output_labels=False)
         return self._train_tf_dataset
 
     @property
-    def test_tf_dataset(self):
+    def test_tf_dataset(self) -> tf.data.Dataset:
         if self._test_tf_dataset is None:
             self._test_tf_dataset = self.make_tf_dataset(output_labels=True)
         return self._test_tf_dataset
 
     @property
-    def shard_count_per_sample(self) -> int:
+    def shards_per_sample(self) -> int:
         return self.config.shard_count_per_sample
 
     @property
     def modalities(self) -> ModalityCollection:
         return self.config.modalities
+
+    @property
+    def source_count(self) -> int:
+        return len(self.subset_folders)
     # endregion
 
 

kitchen_sink.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import cv2
 from typing import List
+from tensorflow.python.framework import errors
 
 from datasets.loaders import DatasetConfig, SubsetLoader
 from modalities import ModalityShape, RawVideo, OpticalFlow, DoG
@@ -86,8 +87,7 @@ def read_and_display_dataset():
                            })
 
     loader = SubsetLoader(config, "Test")
-    dataset = loader.make_tf_dataset(output_labels=True,
-                                     batch_size=1)
+    dataset = loader.make_tf_dataset(output_labels=True).batch(1)
 
     for batch in dataset:
         raw_video, flow, dog, raw_labels = tuple(batch.values())
@@ -130,20 +130,7 @@ def read_and_display_dataset():
         # cv2.waitKey(0)
 
 
-def main():
-    video_io_shape = ModalityShape(input_shape=(32, 128, 128, 1),
-                                   output_shape=(32, 128, 128, 1))
-    config = DatasetConfig(tfrecords_config_folder="../datasets/ucsd/ped2",
-                           modalities_io_shapes=
-                           {
-                               RawVideo: video_io_shape,
-                               OpticalFlow: video_io_shape,
-                               # DoG: video_io_shape
-                           })
-
-    loader = SubsetLoader(config, "Test")
-    dataset = loader.train_tf_dataset.batch(8)
-
+def make_test_model():
     from tensorflow.python.keras.models import Model
     from tensorflow.python.keras.layers import Input, Conv3D, Conv3DTranspose, Flatten, Reshape, Concatenate, Dense
 
@@ -153,14 +140,14 @@ def main():
         layer = Conv3D(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
         layer = Conv3D(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
         layer = Conv3D(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
-        layer = Conv3D(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
+        layer = Conv3D(filters=32, kernel_size=3, strides=(1, 2, 2), padding="same", activation="relu")(layer)
 
         return Flatten()(layer)
 
     def make_decoder(input_layer, output_filters):
         layer = input_layer
         layer = Reshape([1, 4, 4, 32])(layer)
-        layer = Conv3DTranspose(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
+        layer = Conv3DTranspose(filters=32, kernel_size=3, strides=(1, 2, 2), padding="same", activation="relu")(layer)
         layer = Conv3DTranspose(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
         layer = Conv3DTranspose(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
         layer = Conv3DTranspose(filters=32, kernel_size=3, strides=2, padding="same", activation="relu")(layer)
@@ -168,8 +155,8 @@ def main():
                                 activation="sigmoid")(layer)
         return layer
 
-    raw_video_input_layer = Input(shape=(32, 128, 128, 1))
-    optical_flow_input_layer = Input(shape=(32, 128, 128, 2))
+    raw_video_input_layer = Input(shape=(16, 128, 128, 1))
+    optical_flow_input_layer = Input(shape=(16, 128, 128, 2))
 
     raw_video_embeddings = make_encoder(raw_video_input_layer)
     optical_flow_embeddings = make_encoder(optical_flow_input_layer)
@@ -177,8 +164,14 @@ def main():
     embeddings = Concatenate()([raw_video_embeddings, optical_flow_embeddings])
     embeddings = Dense(units=512)(embeddings)
 
-    raw_video_output_layer = make_decoder(embeddings, 1)
-    optical_flow_output_layer = make_decoder(embeddings, 2)
+    raw_video_decoder = make_decoder(embeddings, 1)
+    optical_flow_decoder = make_decoder(embeddings, 2)
+
+    raw_video_predictor = make_decoder(embeddings, 1)
+    optical_flow_predictor = make_decoder(embeddings, 2)
+
+    raw_video_output_layer = Concatenate(axis=1)([raw_video_decoder, raw_video_predictor])
+    optical_flow_output_layer = Concatenate(axis=1)([optical_flow_decoder, optical_flow_predictor])
 
     inputs = [raw_video_input_layer, optical_flow_input_layer]
     outputs = [raw_video_output_layer, optical_flow_output_layer]
@@ -186,11 +179,40 @@ def main():
     model = Model(inputs=inputs, outputs=outputs)
     model.compile("adam", loss="mse")
 
-    model.summary()
+    return model
 
-    model.fit(dataset, steps_per_epoch=2000)
+
+def main():
+    config = DatasetConfig(tfrecords_config_folder="../datasets/ucsd/ped2",
+                           modalities_io_shapes=
+                           {
+                               RawVideo: ModalityShape(input_shape=(16, 128, 128, 1),
+                                                       output_shape=(32, 128, 128, 1)),
+                               OpticalFlow: ModalityShape(input_shape=(16, 128, 128, 1),
+                                                          output_shape=(32, 128, 128, 2)),
+                               # DoG: video_io_shape
+                           })
+
+    loader = SubsetLoader(config, "Test")
+    dataset = loader.get_source_browser(11, RawVideo, 1)
+    # iterator = dataset.make_initializable_iterator()
+    # iterator_next = iterator.get_next()
+
+    for batch in dataset.batch(150):
+        raw_video = np.squeeze(batch["RawVideo"])
+        labels = np.squeeze(batch["labels"], axis=1)
+        # labels = np.reshape(labels, [len(labels), -1])
+        labels_not_equal: np.ndarray = np.abs(labels[:, :, 0] - labels[:, :, 1]) > 1e-7
+        labels_not_equal = np.any(labels_not_equal, axis=-1)
+        for i in range(len(labels)):
+            frame = raw_video[i][-1]
+            if labels_not_equal[i]:
+                frame *= 0.5
+            frame = cv2.resize(frame, (512, 512))
+            cv2.imshow("frame", frame)
+            cv2.waitKey(50)
 
 
 if __name__ == "__main__":
-    # tf.enable_eager_execution()
+    tf.enable_eager_execution()
     main()

models/AutoEncoderBaseModel.py
@@ -1,6 +1,6 @@
 from tensorflow.python.keras.models import Model as KerasModel
 from tensorflow.python.keras.layers import Activation, LeakyReLU, Dense, Dropout, Input
-from tensorflow.python.keras.layers import Layer, Reshape, Concatenate
+from tensorflow.python.keras.layers import Layer, Reshape, Concatenate, Lambda
 from tensorflow.python.keras.layers import BatchNormalization
 from tensorflow.python.keras.layers import Conv3D, Conv3DTranspose, MaxPooling3D, AveragePooling3D, UpSampling3D
 from tensorflow.python.keras.initializers import VarianceScaling
@@ -23,6 +23,7 @@ from tqdm import tqdm
 from typing import List, Union, Dict, Tuple, Optional
 
 from layers import ResBlock3D, ResBlock3DTranspose, DenseBlock3D, SpectralNormalization
+from layers.utility_layers import RawPredictionsLayer
 from datasets import SubsetLoader, DatasetLoader
 from utils.train_utils import get_log_dir
 from utils.summary_utils import image_summary
@@ -184,7 +185,7 @@ class AutoEncoderBaseModel(ABC):
         self._encoder: Optional[KerasModel] = None
         self._decoder: Optional[KerasModel] = None
         self._autoencoder: Optional[KerasModel] = None
-        self._raw_predictions_models: Dict[Tuple, RunModel] = {}
+        self._raw_predictions_models: Dict[Tuple, KerasModel] = {}
         self._reconstructor_model: Optional[RunModel] = None
 
         self.decoder_input_layer = None
@@ -717,8 +718,7 @@ class AutoEncoderBaseModel(ABC):
         self.on_train_begin(dataset)
 
         callbacks = self.build_callbacks(dataset)
-        samples_count = None
-        callbacks = self.setup_callbacks(callbacks, batch_size, epochs, epoch_length, samples_count)
+        callbacks = self.setup_callbacks(callbacks, batch_size, epochs, epoch_length)
 
         callbacks.on_train_begin()
         try:
@@ -840,7 +840,7 @@ class AutoEncoderBaseModel(ABC):
 
         video_writer.release()
 
-    def get_anomalies_raw_predictions_model(self, reduction_axis=(2, 3, 4)) -> RunModel:
+    def get_anomalies_raw_predictions_model(self, reduction_axis=(2, 3, 4)) -> KerasModel:
         if reduction_axis not in self._raw_predictions_models:
             reconstructor = KerasModel(inputs=self.decoder_input_layer, outputs=self.reconstructor_output,
                                        name="Reconstructor")
@@ -848,26 +848,19 @@ class AutoEncoderBaseModel(ABC):
             if isinstance(encoder_output, list):
                 encoder_output = encoder_output[0]
             pred_output = reconstructor(encoder_output)
-            true_output = tf.placeholder(dtype=pred_output.dtype, shape=pred_output.shape)
+            true_output = self.encoder.get_input_at(0)
+            error = RawPredictionsLayer(reduction_axis)([pred_output, true_output])
 
-            error = tf.square(pred_output - true_output)
-            error = tf.reduce_sum(error, axis=reduction_axis)
+            labels_input_layer = Input(shape=[None, 2], dtype=tf.float32, name="labels_input_layer")
+            labels_output_layer = Lambda(tf.identity, name="labels_identity")(labels_input_layer)
 
-            inputs = [self.encoder.get_input_at(0), true_output]
-            outputs = [error]
-            raw_predictions_model = RunModel(inputs, outputs)
+            inputs = [self.encoder.get_input_at(0), labels_input_layer]
+            outputs = [error, labels_output_layer]
+            raw_predictions_model = KerasModel(inputs=inputs, outputs=outputs)
 
             self._raw_predictions_models[reduction_axis] = raw_predictions_model
 
         return self._raw_predictions_models[reduction_axis]
-
-    def predict_anomalies_on_single_example(self, step_video: np.ndarray):
-        raw_predictions_model = self.get_anomalies_raw_predictions_model()
-
-        step_predictions = raw_predictions_model.predict(x=[step_video, step_video], batch_size=1)
-        step_predictions = np.squeeze(step_predictions)
-
-        return step_predictions
 
     # region Using attractors
     def get_reconstructor_run_model(self) -> RunModel:
@@ -920,47 +913,23 @@ class AutoEncoderBaseModel(ABC):
                                    normalize_predictions=False,
                                    convergence_threshold=None,
                                    max_iterations=1):
-        video_length = subset.get_video_length(video_index)
-        steps_count = 1 + (video_length - self.input_sequence_length) // stride
-        # video_length = steps_count * stride
+        from modalities import RawVideo
 
-        # region Scores arrays
-        predictions = np.zeros(shape=[video_length])
-
-        if subset.has_labels:
-            video_labels = subset.get_video_frame_labels(video_index, 0, video_length)
-        else:
-            video_labels = np.zeros(shape=[video_length], dtype=np.bool)
-
-        counts = np.zeros(shape=[video_length], dtype=np.int32)
-        # endregion
-
-        # region Compute scores for video
-        for i in range(steps_count):
-            start = i * stride
-            end = start + self.input_sequence_length
-
-            step_video = subset.get_video_frames(video_index, start, end)
-            step_video = np.expand_dims(step_video, axis=0)
-
-            if convergence_threshold is None:
-                step_predictions = self.predict_anomalies_on_single_example(step_video)
-            else:
-                step_predictions = self.predict_anomalies_on_single_example_until_convergence(step_video,
-                                                                                              convergence_threshold,
-                                                                                              max_iterations)
-
-            predictions[start: end] += step_predictions
-            counts[start:end] += 1
-        # endregion
-
-        predictions = predictions / counts
+        session = get_session()
+        raw_predictions_model = self.get_anomalies_raw_predictions_model()
+        iterator = subset.get_source_browser_iterator(video_index, RawVideo, stride)
+        session.run(iterator.initializer)
+        # TODO : Get steps count
+        steps_count = 1000
+        predictions, labels = raw_predictions_model.predict(iterator.iterator_next, steps=steps_count)
+        labels = np.abs(labels[:, :, 0] - labels[:, :, 1]) > 1e-7
+        labels = np.any(labels, axis=-1)
 
         if normalize_predictions:
             predictions = (predictions - predictions.min()) / predictions.max()
             # predictions = (predictions - predictions.min()) / (predictions.max() - predictions.min())
 
-        return predictions, video_labels
+        return predictions, labels
 
     def predict_anomalies_on_subset(self,
                                     subset: SubsetLoader,
@@ -1042,6 +1011,7 @@ class AutoEncoderBaseModel(ABC):
         session: tf.Session = get_session()
         session.run(tf.local_variables_initializer())
 
+        labels = np.tile(labels[:, np.newaxis], [1, predictions.shape[1]])
         eval_ops = [self._auc_ops, self._auc_summary_ops] if log_in_tensorboard else [self._auc_ops]
         eval_results = session.run(eval_ops, feed_dict={self._auc_predictions_placeholder: predictions,
                                                         self._auc_labels_placeholder: labels})
@@ -1228,7 +1198,9 @@ class AutoEncoderBaseModel(ABC):
                                      tensorboard: TensorBoard,
                                      frequency="epoch",
                                      include_composite=False) -> ImageCallback:
-        videos = subset.get_batch(batch_size=self.image_summaries_max_outputs, output_labels=False)
+        inputs, outputs = subset.get_batch(batch_size=self.image_summaries_max_outputs, output_labels=False)
+        # TODO : Switch to dict to use outputs[RawVideo]
+        videos = [inputs[0], outputs[0]]
 
         true_outputs_placeholder = self.get_true_outputs_placeholder()
         summary_inputs = [self.autoencoder.input, true_outputs_placeholder]
@@ -1309,15 +1281,13 @@ class AutoEncoderBaseModel(ABC):
                         callbacks: CallbackList or List[Callback],
                         batch_size: int,
                         epochs: int,
-                        epoch_length: int,
-                        samples_count: int) -> CallbackList:
+                        epoch_length: int) -> CallbackList:
         callbacks = CallbackList(callbacks)
         callbacks.set_model(self.autoencoder)
         callbacks.set_params({
             'batch_size': batch_size,
             'epochs': epochs,
             'steps': epoch_length,
-            'samples': samples_count,
             'verbose': 1,
             'do_validation': True,
             'metrics': self.callback_metrics(),
@@ -1339,6 +1309,7 @@ class AutoEncoderBaseModel(ABC):
     def make_log_dir(cls,
                      dataset: DatasetLoader):
         project_log_dir = "../logs/AutoEncoding-Anomalies"
+        # TODO : Change the way Dataset name is chosen (no longer dataset.__class__.__name__)
         base_dir = os.path.join(project_log_dir, dataset.__class__.__name__, cls.__name__)
         log_dir = get_log_dir(base_dir)
         return log_dir