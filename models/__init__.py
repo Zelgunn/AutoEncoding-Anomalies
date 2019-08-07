b20d85a7399a1c27153376ee49bf331d79e34dcb
Valentin Durand de Gevigney <valentin.durand.de.g@gmail.com>
2019-08-07 15:23:38 +0200

    Removed all files within `models` folder and removed all code in main This is to do clean slate, as previous models didn't follow with recent improvements

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from models.AutoEncoderBaseModel import metrics_dict, conv_type, LayerBlock, LayerStack
-from models.AutoEncoderBaseModel import AutoEncoderBaseModel
-from models.VariationalBaseModel import VariationalBaseModel
-from models.BasicAE import BasicAE
-from models.AGE import AGE
-from models.VAE import VAE
-from models.GAN import GAN
-from models.VAEGAN import VAEGAN
