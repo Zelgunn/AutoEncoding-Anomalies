c39791d08d8c21c34c0a63143e64e5ca2d6ae581
Zelgunn <valentin.durand.de.g@gmail.com>
2020-09-22 14:07:25 +0200

    ped2.json : Switched to tanh output activation VideoProtocol.py : Increased "description_energy_loss" weight (from 1e-3 to 1e-2)

--- protocols/VideoProtocol.py
+++ protocols/VideoProtocol.py
@@ -178,7 +178,7 @@ class VideoProtocol(DatasetProtocol):
                      features_per_block=1,
                      merge_dims_with_features=False,
                      add_binarization_noise_to_mask=True,
-                     description_energy_loss_lambda=1e-3,
+                     description_energy_loss_lambda=1e-2,
                      seed=self.seed)
         return model
 
