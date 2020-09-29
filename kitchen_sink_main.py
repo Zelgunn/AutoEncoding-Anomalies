249f4045933630671f762715e78d905d3b4af6a5
Valentin Durand de Gevigney <valentin.durand.de.g@gmail.com>
2020-09-29 16:32:32 +0200

    Merged from remote

--- kitchen_sink_main.py
+++ kitchen_sink_main.py
@@ -1,56 +1,21 @@
-from kitchen_sink.audioset_ebl3 import main
 import tensorflow as tf
-from time import time
+
+from protocols.video_protocols import UCSDProtocol
 
 
-def tmp_main():
-    x = tf.Variable(initial_value=0.1)
-    y = tf.Variable(initial_value=1.0)
+def main():
+    protocol = UCSDProtocol(initial_epoch=0, dataset_version=2)
 
-    with tf.GradientTape() as tape:
-        z = x * y
-        loss = tf.abs(z + 0.5)
+    model = protocol.make_model()
 
-    print(tape.gradient(loss, [x, y]))
-
-
-def compare_einsum_matmul():
-    seed = 42
-    batch_size = 8
-    count = 16 ** 2
-    heads = 8
-    head_size = 16
-    k = 3
-
-    x = tf.random.normal(shape=[batch_size, heads, head_size, k ** 2, count], seed=seed)
-    y = tf.random.normal(shape=[batch_size, count, heads, 1, k ** 2], seed=seed)
-
-    t0 = time()
-    for _ in range(100):
-        _ = test_einsum(x, y)
-
-    t1 = time()
-    print("einsum : {}".format(round(t1 - t0, 2)))
-
-    for _ in range(100):
-        _ = test_matmul_transpose(x, y)
-
-    t2 = time()
-    print("matmul_transpose : {}".format(round(t2 - t1, 2)))
-
-
-@tf.function
-def test_einsum(x, y):
-    # noinspection SpellCheckingInspection
-    return tf.einsum("bhskc,bchsk->bhsc", x, y)
-
-
-@tf.function
-def test_matmul_transpose(x, y):
-    x = tf.transpose(x, perm=[0, ])
+    for layer in model.layers:
+        print("|====| ", layer)
+        if isinstance(layer, tf.keras.models.Model):
+            for sub_layer in layer.layers:
+                print(sub_layer)
+                if hasattr(sub_layer, "kernel_initializer"):
+                    print(sub_layer.kernel_initializer)
 
 
 if __name__ == "__main__":
-    tmp_main()
-    exit()
     main()
