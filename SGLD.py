AnomalyDetectorCallback.py : Fixed a bug where the wrong value would be displayed in tensorboard if multiple metrics were used

SGLD.py
@@ -10,6 +10,7 @@ class SGLD(tf.optimizers.Optimizer):
                  burn_in_steps=25,
                  preconditioner_decay_rate=0.95,
                  diagonal_bias=1e-8,
+                 seed=None,
                  name="SGLD",
                  **kwargs
                  ):
@@ -22,6 +23,7 @@ class SGLD(tf.optimizers.Optimizer):
         self._set_hyper("burn_in_steps", burn_in_steps)
         self._set_hyper("preconditioner_decay_rate", preconditioner_decay_rate)
         self._set_hyper("diagonal_bias", diagonal_bias)
+        self._set_hyper("seed", seed)
 
     def _create_slots(self, var_list):
         for var in var_list:
@@ -86,7 +88,8 @@ class SGLD(tf.optimizers.Optimizer):
         # noise_stddev = tf.sqrt(noise_stddev)
 
         noisy_gradient = tf.random.normal(mean=gradient_mean, stddev=noise_stddev,
-                                          shape=gradient_shape, dtype=gradient_dtype)
+                                          shape=gradient_shape, dtype=gradient_dtype,
+                                          seed=self._get_hyper("seed"))
 
         if indices is None:
             momentum.assign(new_momentum)

kitchen_sink_main.py
@@ -15,14 +15,15 @@ def tmp_main():
 
 
 def compare_einsum_matmul():
+    seed = 42
     batch_size = 8
     count = 16 ** 2
     heads = 8
     head_size = 16
     k = 3
 
-    x = tf.random.normal(shape=[batch_size, heads, head_size, k ** 2, count])
-    y = tf.random.normal(shape=[batch_size, count, heads, 1, k ** 2])
+    x = tf.random.normal(shape=[batch_size, heads, head_size, k ** 2, count], seed=seed)
+    y = tf.random.normal(shape=[batch_size, count, heads, 1, k ** 2], seed=seed)
 
     t0 = time()
     for _ in range(100):

protocols/VideoProtocol.py
@@ -19,9 +19,7 @@ from data_processing.video_preprocessing import make_video_augmentation, make_vi
 
 from data_processing.video_processing.VideoPatchExtractor import VideoPatchExtractor
 
-
-# from misc_utils.train_utils import ScaledSchedule
-from misc_utils.train_utils import WarmupSchedule
+from misc_utils.train_utils import CyclicSchedule, ScaledSchedule
 
 
 class VideoProtocol(DatasetProtocol):
@@ -569,7 +567,11 @@ class VideoProtocol(DatasetProtocol):
     def base_learning_rate_schedule(self):
         learning_rate = self.learning_rate
         # learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(learning_rate, 2000, 0.8, staircase=False)
-        learning_rate = WarmupSchedule(warmup_steps=1000, learning_rate=learning_rate)
+        # learning_rate = WarmupSchedule(warmup_steps=2000, learning_rate=learning_rate)
+        min_learning_rate = ScaledSchedule(learning_rate, 1e-2)
+        learning_rate = CyclicSchedule(cycle_length=1000,
+                                       learning_rate=min_learning_rate,
+                                       max_learning_rate=learning_rate)
         return learning_rate
 
     @property